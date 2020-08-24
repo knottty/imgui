6b972654b53aa2569fc934cb257b6f8b45eb2cf5
Rokas Kupstys <rokups@zoho.com>
2020-08-24 11:59:28 +0300

    ImStrv: support for test/automation framework

--- imgui.cpp
+++ imgui.cpp
@@ -2978,7 +2978,8 @@ ImGuiID ImGuiWindow::GetID(ImStrv str)
     ImGui::KeepAliveID(id);
 #ifdef IMGUI_ENABLE_TEST_ENGINE
     ImGuiContext& g = *GImGui;
-    IMGUI_TEST_ENGINE_ID_INFO2(id, ImGuiDataType_String, str, str_end);
+    IM_IMSTR_ENSURE_HAS_END(str);
+    IMGUI_TEST_ENGINE_ID_INFO2(id, ImGuiDataType_String, str.Begin, str.End);
 #endif
     return id;
 }
@@ -3013,7 +3014,8 @@ ImGuiID ImGuiWindow::GetIDNoKeepAlive(ImStrv str)
     ImGuiID id = ImHashStr(str, seed);
 #ifdef IMGUI_ENABLE_TEST_ENGINE
     ImGuiContext& g = *GImGui;
-    IMGUI_TEST_ENGINE_ID_INFO2(id, ImGuiDataType_String, str, str_end);
+    IM_IMSTR_ENSURE_HAS_END(str);
+    IMGUI_TEST_ENGINE_ID_INFO2(id, ImGuiDataType_String, str.Begin, str.End);
 #endif
     return id;
 }
